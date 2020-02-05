eb03eee398ee75655d2d32c63729cea9c1b7c6d5
g00364842 <g00364842@gmit.ie>
2020-02-05 16:06:09 +0000

    Added js css javaScript

--- index.php
+++ index.php
@@ -1,10 +1,14 @@
-
 <html>
-
-<head>
-</head>
-<body>
-
-    <h1>My Website</h1>
-    <p id=demo>hello</p>
+    <head>
+        <link rel="stylesheet" href="styles.css">
+        
+    </head>
+    <body>
+            <h1>My Website</h1>
+            <p id=demo>Hello World!</p>
+            <?php echo '<p>Hello World! PHP</p>'; ?>
+            <script type="text/javascript" src="myscripts.js"></script>
+            <button onclick="myFunction(demo);">Click Me to turn text blue</button>
+            
+    </body>
 </html>
